39893cb5571fa8903c698f5c883835a9a3f7ad0e
Vittor Fonseca Serra <vittorserradev@gmail.com>
2023-08-15 21:08:15 -0300

    Mudei a cor do preço dos produtos

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@ export default function Product(){
                 <div className="flex justify-end items-start">
                     <img className="2xl:h-[300px] 2xl:w-[300px] lg:h-[280px] lg:w-[280px] md:h-[240px] md:w-[240px] object-cover" src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                     <div className="absolute mt-5 mr-3">
-                        <span className="pr-[5px] pl-[5px] pt-[10px] pb-[10px] rounded-[3px] bg-red-500 text-white font-semibold text-base">R$ 260,90</span>
+                        <span className="pr-[5px] pl-[5px] pt-[10px] pb-[10px] rounded-[3px] bg-[#FE0000] text-white font-semibold text-base">R$ 260,90</span>
                     </div>
                 </div>
                 <div className="bg-black 2xl:w-[300px]  lg:w-[280px] md:w-[240px] w-full p-3.5 flex flex-col gap-2.5 items-center justify-center ">
